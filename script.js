94171e0298db3317123cb196e0cf278c44e81f12
GiaBab <gabrielbianglino@gmail.com>
2023-05-11 19:57:18 -0300

    sistema de modo oscuro mejorado

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
 const btnSwitch = document.querySelector('.switch');
-const body = document.querySelector('body');
+const $body = document.querySelector('body')
+const navList = document.querySelectorAll('.nav-link')
+
+function swapImages()
+{
+    var img = document.querySelector('#switchImg') ;
+    if(img.getAttribute('src') == 'modoOscuro.png')
+    {
+        img.setAttribute('src', 'modoLuminoso.png') ;
+    }
+    else
+    {
+        img.setAttribute('src', 'modoOscuro.png') ;
+    }
+}
 
 btnSwitch.addEventListener('click', () => 
     {
-        body.classList.toggle('bg-light') ;
-        body.classList.toggle('text-dark') ;
-        body.classList.toggle('bg-dark') ;
-        body.classList.toggle('text-white');
+        swapImages() ;
+        $body.classList.toggle('bg-light') ;
+        $body.classList.toggle('text-dark') ;
+        $body.classList.toggle('bg-dark') ;
+        $body.classList.toggle('text-white');
         btnSwitch.classList.toggle('active') ;
+        navList.forEach(a => a.classList.toggle('text-white'));
     })
